c61b335dcb9a583be778a44049a34029c70c6bb9
khoursaaf <khoursa@gmail.com>
2018-11-20 16:22:20 +0100

    correction

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Créer une variable isEasy de type booléan et l'initialiser avec une valeur. Afficher C'est facile!! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!.</p>
-      <?php
-      $isEasy = true;
-      if ($isEasy = false) {
-        echo "C'est difficile!!!";
-      } else {
-        echo "C'est facile!!";
-      }
-       ?>
-      <p>Bonus : L'écrire de deux manières différentes.</p>
-      <?php
-      $isEasy = false;
-      if (!$isEasy) {
-        echo "C'est difficile!!!";
-      } else {
-        echo "C'est facile!!";
-      }
-       ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer une variable isEasy de type booléan et l'initialiser avec une valeur. Afficher C'est facile!! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!.</p>
+    <?php
+    $isEasy = rand(true, false);
+    if ($isEasy = false) {
+      echo "C'est difficile!!!";
+    } else {
+      echo "C'est facile!!";
+    }
+    ?>
+    <p>Bonus : L'écrire de deux manières différentes.</p>
+    <p><?php echo($isEasy == true)? "Cest facile" : "C'est difficile" ;?></p>
+  </body>
 </html>
